f76697c25ee6ec9c3a3e230ffab6a7f06d1a7407
Danilo-ssv <danilosouza20033002@gmail.com>
2023-08-13 19:42:28 -0300

    'cors-config3'

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cors())
 
-app.use((req, res, next)=>{
+app.use('/', (req, res, next)=>{
 	res.header('Access-Control-Allow-Origin',
 	'https://armazem-client.vercel.app')
 
 	next()
-})
-app.use('/', router)
+}, router)
 
 mongoose.connect(process.env.DATABASE_CONNECTION, {
 	useNewUrlParser: true,
